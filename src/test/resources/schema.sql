f4e2e5a3a249577fed1abe3bb31223eb00791c44
franciscoseita <105219328+franciscoseita@users.noreply.github.com>
2024-12-05 10:53:35 -0600

    Update schema.sql

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,10 +1,4 @@
 -- Crear la base de datos si no existe
-CREATE DATABASE IF NOT EXISTS my_database;
-
--- Usar la base de datos
-USE my_database;
-
--- Crear la tabla si no existe
 CREATE TABLE IF NOT EXISTS usuarios (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) DEFAULT NULL,
