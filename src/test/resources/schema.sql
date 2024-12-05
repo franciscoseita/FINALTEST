48698750939c12bbb43100534f473a9c0197bc43
franciscoseita <franciscoseita2001@gmail.com>
2024-12-05 10:34:47 -0600

    first commit

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,14 @@
+-- Crear la base de datos si no existe
+CREATE DATABASE IF NOT EXISTS my_database;
+
+-- Usar la base de datos
+USE my_database;
+
+-- Crear la tabla si no existe
+CREATE TABLE IF NOT EXISTS usuarios (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) DEFAULT NULL,
+    email VARCHAR(30) DEFAULT NULL,
+    password VARCHAR(16) DEFAULT NULL,
+    isLogged TINYINT(1) DEFAULT NULL
+);
